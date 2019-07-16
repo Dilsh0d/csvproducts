2806fb32d9273c91f9a19fe205787d320824f280
dilsh0d <dilshod.toj@gmail.com>
2019-07-16 13:34:12 +0500

    Merchant time table done!

--- src/com/csv/products/CreateCsvFile.java
+++ src/com/csv/products/CreateCsvFile.java
@@ -13,18 +13,18 @@ import java.util.List;
 public class CreateCsvFile {
     private static final char DEFAULT_SEPARATOR = ';';
 
-    public static void main(String[] args) throws IOException {
-        float min = 100f,max = 100_000f;
-        for (int file=0;file<100;file++) {
-            FileWriter writer = new FileWriter(args[0] + File.separator + String.valueOf(file) + ".csv");// D:\csv-files
-            for(int row=0;row<1_000_000;row++){
-                double price = min + Math.random() * (max - min);
-                writeLine(writer, Arrays.asList(String.valueOf(row), "Product Name"+row, "Condation"+row, "State"+row,String.valueOf(Math.floor(price))));// Each file contains 5 columns: product ID (integer), Name (string), Condition (string), State (string), Price (float).
-            }
-            writer.flush();
-            writer.close();
-        }
-    }
+//    public static void main(String[] args) throws IOException {
+//        float min = 100f,max = 100_000f;
+//        for (int file=0;file<100;file++) {
+//            FileWriter writer = new FileWriter(args[0] + File.separator + String.valueOf(file) + ".csv");// D:\csv-files
+//            for(int row=0;row<1_000_000;row++){
+//                double price = min + Math.random() * (max - min);
+//                writeLine(writer, Arrays.asList(String.valueOf(row), "Product Name"+row, "Condation"+row, "State"+row,String.valueOf(Math.floor(price))));// Each file contains 5 columns: product ID (integer), Name (string), Condition (string), State (string), Price (float).
+//            }
+//            writer.flush();
+//            writer.close();
+//        }
+//    }
 
     public static void writeLine(Writer w, List<String> values) throws IOException {
         writeLine(w, values, DEFAULT_SEPARATOR, ' ');
